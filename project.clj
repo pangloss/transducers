0863c156d7fcfbf0c656b792c9863a5de89d222f
Darrick Wiebe <dw@xnlogic.com>
2015-08-17 15:19:04 -0400

    Properly name things

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject xn.transducers "0.1.0"
+(defproject com.xnlogic/transducers "0.1.0"
   :description "Useful transducers for both Clojure and ClojureScript"
   :url "http://xnlogic.com"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.7.0"]])
+  :dependencies [[org.clojure/clojure "1.7.0" :scope "provided"]])
